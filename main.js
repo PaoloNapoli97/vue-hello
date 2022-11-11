f8cf437464a598ef4b08d93b98484b50aecf028b
PaoloNapoli97 <paolonapoli@live.it>
2022-11-11 16:37:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return{
+            ciao: {
+                message: 'HEEEEEELLLOOO CLOCHARD',
+                spaidermen: '01.webp'
+            }
+        } 
+    }
+}).mount('#hello')
